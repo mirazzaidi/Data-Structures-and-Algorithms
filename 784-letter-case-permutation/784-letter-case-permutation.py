504a5b8089acaedbdda1ff8ef395cd033f68ccff
Miraz Zaidi <miraz.zaidi@gmail.com>
2022-09-08 20:23:59 +0200

    Time: 128 ms (16.21%), Space: 15.3 MB (46.82%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.py
+++ 784-letter-case-permutation/784-letter-case-permutation.py
@@ -1,12 +1,7 @@
 class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
 
-        def flip(char):
-            if char.isupper():
-                return char.lower()
-            else:
-                return char.upper()
-            
+
         def permute(index: int, curr):
             if index == len(s):
                 res.append(curr)
@@ -15,7 +10,7 @@ class Solution:
             permute(index+1, curr + s[index])
 
             if s[index] not in '0123456789':
-                permute(index+1, curr + flip(s[index]))
+                permute(index+1, curr + (s[index]).swapcase())
                 
         res = []
         permute(0, curr = '')
