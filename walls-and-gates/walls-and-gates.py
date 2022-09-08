6687d727a1c7fc248ae1fe16f5344f6e09b82a62
Miraz Zaidi <miraz.zaidi@gmail.com>
2022-09-08 23:24:23 +0200

    Update walls-and-gates.py

--- walls-and-gates/walls-and-gates.py
+++ walls-and-gates/walls-and-gates.py
@@ -9,28 +9,22 @@ class Solution:
     """
     INF = 2147483647
     def walls_and_gates(self, rooms: List[List[int]]):
-        # write your code here
+        def add_room(row, col, distance):
+            if 0 <= row < len(rooms) and 0 <= col < len(rooms[0]) and rooms[row][col] == self.INF:
+                rooms[row][col] = distance
+                q.append((row, col))
+        
         q = deque()
         for i in range(len(rooms)):
             for j in range(len(rooms[0])):
                 if rooms[i][j] == 0:
                     q.append((i,j))
-        
-        directions = [(-1,0), (0,1), (1,0), (0,-1)]
         while q:
             count = len(q)
             for _ in range(count):
                 row, col = q.popleft()
-                distance = rooms[row][col]
-                
-                for direction in directions:
-                    next_row = row + direction[0]
-                    next_col = col + direction[1]
-
-                    if (0 <= next_row < len(rooms)) and (0 <= next_col < len(rooms[0])):
-                        if rooms[next_row][next_col] == self.INF:
-
-                            rooms[next_row][next_col] = rooms[row][col] + 1
-                            q.append((next_row, next_col))
-            
-
+                distance = rooms[row][col] + 1
+                add_room(row-1, col, distance)
+                add_room(row, col-1, distance)
+                add_room(row+1, col, distance)
+                add_room(row, col+1, distance)
