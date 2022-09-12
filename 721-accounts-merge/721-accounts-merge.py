9418ca2f03af4b3d7397918577a4650b37a3d779
Miraz Zaidi <miraz.zaidi@gmail.com>
2022-09-13 01:01:16 +0200

    Time: 542 ms (19.50%), Space: 22.9 MB (28.41%) - LeetHub

--- 721-accounts-merge/721-accounts-merge.py
+++ 721-accounts-merge/721-accounts-merge.py
@@ -2,9 +2,10 @@ class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
         
         def dfs(key, connection):
+            
             for node in graph[key]:
-                if not visited.get(node, False):
-                    visited[node] = True
+                if node not in visited:
+                    visited.add(node)
                     connection.append(node)
                     dfs(node, connection)
             
@@ -21,30 +22,24 @@ class Solution:
             if email_key not in graph:
                 graph[email_key] = []
 
-            for email2 in emails[1:]:
-                key2 = email2
-                if key2 not in graph:
-                    graph[key2] = []                
-                graph[email_key].append(key2)
-                graph[key2].append(email_key)
+            for email in emails[1:]:
+                if email not in graph:
+                    graph[email] = []                
+                graph[email_key].append(email)
+                graph[email].append(email_key)
         
-        visited = {}
-        connections = []
+        visited = set()
+        merged_accounts = []
+        
         for account in accounts:
             account_name = account[0]
             emails = account[1:]
             email_key = emails[0]
             
-            if not visited.get(email_key, False):
-                visited[email_key] = True
+            if email_key not in visited:
+                visited.add(email_key)
                 emails = [email_key]
-                
                 dfs(email_key, emails)
-                
-                
-                connections.append([account_name, *sorted(emails)])
-        
-        return connections
-
-        
-        
+                emails.sort()
+                merged_accounts.append([account_name, *emails])
+        return merged_accounts
