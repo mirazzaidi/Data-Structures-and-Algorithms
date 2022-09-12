3eabc53565f386296df4876b4a9591681f781429
Miraz Zaidi <miraz.zaidi@gmail.com>
2022-09-12 17:34:29 +0200

    Time: 122 ms (18.04%), Space: 14 MB (37.07%) - LeetHub

--- 929-unique-email-addresses/929-unique-email-addresses.py
+++ 929-unique-email-addresses/929-unique-email-addresses.py
@@ -5,10 +5,7 @@ class Solution:
         
         for email in emails:
             local_name, domain_name = email.split('@')
-           
-            local_name = local_name.replace('.','')
-           
-                
             local_name = local_name.split('+')[0]
+            local_name = local_name.replace('.','')    
             output.add(local_name+'@'+domain_name)
         return len(output)
